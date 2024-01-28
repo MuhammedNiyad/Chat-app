08be1a8a36c125717747a8cadc49ea35778531ca
Muhammed Niyad <niyadpunniyath007@gmail.com>
2024-01-28 20:46:57 +0530

    created frondend

--- Backend/index.js
+++ Backend/index.js
@@ -1,27 +1,28 @@
 const express = require('express');
 const chats = require('./Data/data');
 const dotenv = require('dotenv');
+const cors = require("cors");
 const app = express();
 
-
+app.use(cors());
 dotenv.config();
 
 
-const PORT = process.env.PORT || 3000
 
-    app.get('/',(req, res)=>{
+app.get('/',(req, res)=>{
         res.send("Hello Niyad.....!")
     });
-
+    
     app.get('/api/chat', (req, res)=>{
         res.send(chats);
     });
-
+    
     app.get('/api/chat/:id', (req, res)=> {
         // console.log(req.params.id);
         const singleChat = chats.find((c)=>c._id === req.params.id);
         res.send(singleChat);
     });
 
-
-app.listen(PORT, console.log('Server running on PORT :',PORT))
+    
+    const PORT = process.env.PORT || 3000
+    app.listen(PORT, console.log('Server running on PORT :',PORT))
---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import { Button } from '@chakra-ui/react';
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <Button colorScheme="green">Button</Button>
+    </div>
+  );
+}
